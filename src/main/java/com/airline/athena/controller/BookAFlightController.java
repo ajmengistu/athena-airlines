68109b09b8d3295f7301428bb0b41f6935ea491a
ajmengistu <a_mengistu@outlook.com>
2019-05-30 18:31:15 -0400

    Added Airport model & application controller

---
+++ src/main/java/com/airline/athena/controller/BookAFlightController.java
@@ -0,0 +1,8 @@
+package com.airline.athena.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BookAFlightController {
+
+}
---
+++ src/main/java/com/airline/athena/model/Airport.java
@@ -0,0 +1,43 @@
+package com.airline.athena.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity(name="airports")
+public class Airport {
+	@Column(length = 3, nullable = false, unique = true)
+	private String airportId;
+
+	@Column(length = 20, nullable = false)
+	private String city;
+
+	@Column(length = 100, nullable = false)
+	private String name;
+
+	public Airport() {		
+	}
+	
+	public String getAirportId() {
+		return airportId;
+	}
+
+	public void setAirportId(String airportId) {
+		this.airportId = airportId;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
