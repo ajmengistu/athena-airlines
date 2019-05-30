39ec8bbe6d2fac9c0e01aff2dd9b34d5ba3f8a72
ajmengistu <a_mengistu@outlook.com>
2019-05-30 17:35:54 -0400

    Initial Spring Boot web app commit

---
+++ src/main/java/com/airline/athena/AthenaAirlinesApplication.java
@@ -0,0 +1,13 @@
+package com.airline.athena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthenaAirlinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthenaAirlinesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airline/athena/AthenaAirlinesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.airline.athena;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AthenaAirlinesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
