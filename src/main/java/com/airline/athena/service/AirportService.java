cbb0920dbcf0c02a37ec57ebdcbbbc237163bde2
ajmengistu <a_mengistu@outlook.com>
2019-06-02 19:56:51 -0400

    Added AirportService class with AirportService::getAll()

---
+++ src/main/java/com/airline/athena/service/AirportService.java
@@ -0,0 +1,25 @@
+package com.airline.athena.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.airline.athena.model.Airport;
+import com.airline.athena.repository.AirportRepository;
+
+public class AirportService {
+	@Autowired
+	private AirportRepository airportRepository;
+
+	public List<String> getAll() {
+		List<String> airportIds = new ArrayList<>();
+		List<Airport> airports = airportRepository.findAll();
+
+		for (Airport airport : airports) {
+			airportIds.add(airport.getAirportId() + " - " + airport.getCity());
+		}
+
+		return airportIds;
+	}
+}
